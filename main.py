2e2f80b0f272f0f0e885ccd7077c4275dc2861b9
Joao Fortunato <joaomlfortunato@gmail.com>
2020-06-22 17:32:14 +0100

    Function parse_expression Implemented
    Transforms expression string in a list of elements of that math 
    expression.

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 #!/home/joaofortunato/.virtualenvs/calculator/bin/python3.6
 
+def parse_expression(expression):
+    """Receive math expression and returns list of items.
+
+    Each item is an element of the expression.
+    Example: input: "1+2*2" >> output: ["1","+","2","*","2"]
+    """
+    parsed_expression = []
+
+    for character in expression:
+        if character.isdigit():
+            if parsed_expression[-1].isdigit():
+                parsed_expression[-1] += character
+            else:
+                parse_expression.append(character)
+        elif character in "-+*/()":
+            parse_expression.append(character)
+
+    return parsed_expression
+
+
+def calculator(expression):
+    parsed_exp = parse_expression(expression)
+
+    return result
+
 def main():
     """Receives an arithmetic expression, parses, evaluates & shows result."""
     print("Arithmetic Calculator\n")
@@ -12,6 +37,9 @@ def main():
     while expression != "exit":
 
         expression = input("Insert expression (\"exit\" to quit the app)>>>")
+        result = calculator(expression)
+
+        print("Result:"+result)
 
 
 if __name__ == "__main__":
