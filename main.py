34f9d026ec5fc0fb003352c70a87e99f3f513d59
Joao Fortunato <joaomlfortunato@gmail.com>
2020-06-22 21:54:05 +0100

    Infix to Postfix Notation: The Shunting Yard Algorithm

--- main.py
+++ main.py
@@ -20,10 +20,57 @@ def parse_expression(expression):
     return parsed_expression
 
 
+def infix2postfix(infix_expression):
+    """Infix notation to Postfix notation.
+
+    Receive a list of math expression elements in infix notation and
+    convert to postfix notation using The Shunting Yard Algorithm.
+    Example: input: ["2","+","1","*","3"] >> output ["2","1","3","*","+"]
+    """
+    postfix_expression = []
+    operators = []
+    high_precedence = "*/"
+    low_precedence = "+-"
+
+    for element in infix_expression:
+        if element.isnumeric():
+            postfix_expression.append(element)
+        elif element == "(":
+            operators.append(element)
+        elif element == ")":
+            while operators[-1] != "(":
+                postfix_expression.append(operators[-1])
+                operators.pop()
+            operators.pop()
+        elif element in "+-/*":
+            if not operators or operators[-1] == "(":
+                operators.append(element)
+            elif element in high_precedence and\
+                    operators[-1] is low_precedence:
+                operators.append(element)
+            while element in high_precedence or\
+                    (element is low_precedence and
+                     operators[-1] is high_precedence):
+                postfix_expression.append(operators[-1])
+                operators.pop()
+
+    while operators:
+        if operators[-1] in "()":
+            operators.pop()
+        postfix_expression.append(operators[-1])
+        operators.pop()
+
+    return postfix_expression
+
+
 def calculator(expression):
-    parsed_exp = parse_expression(expression)
+
+    expression_list = parse_expression(expression)
+
+    postfix_expression = infix2postfix(expression_list)
 
     return result
+
 
 def main():
     """Receives an arithmetic expression, parses, evaluates & shows result."""
